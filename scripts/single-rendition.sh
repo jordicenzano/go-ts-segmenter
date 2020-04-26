d43757b4d7ebfbc25acbc87f60869b1f3ebbb0f7
Jorge Cenzano <jcenzano@fb.com>
2020-04-26 10:46:01 -0400

    Added text to single rendition script

--- scripts/single-rendition.sh
+++ scripts/single-rendition.sh
@@ -2,6 +2,8 @@
 
 BASE_DIR="../results/singlerendition"
 
+TEXT="SOURCE-"
+
 # Clean up
 echo "Restarting ${BASE_DIR} directory"
 rm -rf $BASE_DIR/*
@@ -36,7 +38,7 @@ echo "Started manifest-generator for 720p as PID $PID_720p"
 ffmpeg -hide_banner -y \
 -f lavfi -re -i smptebars=size=1280x720:rate=30 \
 -f lavfi -i sine=frequency=1000:sample_rate=48000 -pix_fmt yuv420p \
--vf "drawtext=fontfile=$FONT_PATH: text=\'MAIN 720p - Local time %{localtime\: %Y\/%m\/%d %H.%M.%S} (%{n})\': x=100: y=20: fontsize=40: fontcolor=pink: box=1: boxcolor=0x00000099" \
+-vf "drawtext=fontfile=$FONT_PATH: text=\'${TEXT} 720p - Local time %{localtime\: %Y\/%m\/%d %H.%M.%S} (%{n})\': x=100: y=20: fontsize=40: fontcolor=pink: box=1: boxcolor=0x00000099" \
 -c:v libx264 -b:v 6000k -g 60 -profile:v baseline -preset veryfast \
 -c:a aac -b:a 48k \
 -f mpegts "$BASE_DIR/fifo-720p"
