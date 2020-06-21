566d847ad9f87b029e60e6dd54267bf52061dc00
Jorge Cenzano <jcenzano@fb.com>
2020-06-21 11:52:26 -0400

    Script optimizations

--- scripts/transmuxed-rtmp-server.sh
+++ scripts/transmuxed-rtmp-server.sh
@@ -18,6 +18,7 @@ RTMP_STREAM="${4:-"stream"}"
 HOST_DST="${5:-"localhost:9094"}"
 
 PATH_NAME="srrtmp"
+STREAM_NAME="source"
 BASE_DIR="../results/${PATH_NAME}"
 
 # Clean up
@@ -26,14 +27,15 @@ rm -rf $BASE_DIR/*
 mkdir -p $BASE_DIR
 
 # Create master playlist (this should be created after 1st chunk is uploaded)
+# Assuming source is 1280x720@6Mbps
 echo "Creating master playlist manifest (playlist.m3u8)"
 echo "#EXTM3U" > $BASE_DIR/playlist.m3u8
 echo "#EXT-X-VERSION:3" >> $BASE_DIR/playlist.m3u8
-echo "#EXT-X-STREAM-INF:BANDWIDTH=996000,RESOLUTION=854x480" >> $BASE_DIR/playlist.m3u8
-echo "480p.m3u8" >> $BASE_DIR/playlist.m3u8
+echo "#EXT-X-STREAM-INF:BANDWIDTH=6000000,RESOLUTION=1280x720" >> $BASE_DIR/playlist.m3u8
+echo "$STREAM_NAME.m3u8" >> $BASE_DIR/playlist.m3u8
 
 # Upload master playlist
-curl "https://${HOST_DST}/${PATH_NAME}/playlist.m3u8" -H "Content-Type: application/vnd.apple.mpegurl" --insecure --upload-file $BASE_DIR/playlist.m3u8
+curl "http://${HOST_DST}/${PATH_NAME}/playlist.m3u8" -H "Content-Type: application/vnd.apple.mpegurl" --upload-file $BASE_DIR/playlist.m3u8
 
 # Select font path based in OS
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -43,33 +45,33 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # Creates pipes
-mkfifo $BASE_DIR/fifo-480p
+FIFO_FILENAME="fifo-$STREAM_NAME"
+mkfifo "$BASE_DIR/fifo-$STREAM_NAME"
 
 # Creates hls producer
-# 
-cat "$BASE_DIR/fifo-480p" | ../bin/manifest-generator -lf ../logs/segmenter480p.log -insecure -protocol https -p ${PATH_NAME} -manifestDestinationType 2 -mediaDestinationType 2 -t 1 -l 3 -f 480p_ -cf 480p.m3u8 &
-PID_480p=$!
-echo "Started manifest-generator for 480p as PID $PID_480p"
+cat "$BASE_DIR/$FIFO_FILENAME" | ../bin/manifest-generator -lf ../logs/segmenterSource.log -p ${PATH_NAME} -manifestDestinationType 2 -mediaDestinationType 2 -t 1 -l 3 -f ${STREAM_NAME}_ -cf ${STREAM_NAME}.m3u8 &
+PID_SOURCE=$!
+echo "Started manifest-generator for $STREAM_NAME as PID $PID_SOURCE"
 
 if [[ "$MODE" == "test" ]]; then
     # Start test signal
     # GOP size = 30f @ 30 fps = 1s
     ffmpeg -hide_banner -y \
-    -f lavfi -re -i smptebars=duration=6000:size=1280x720:rate=30 \
-    -f lavfi -i sine=frequency=1000:duration=6000:sample_rate=48000 -pix_fmt yuv420p \
-    -vf scale=854x480 \
-    -vf "drawtext=fontfile=$FONT_PATH:text=\'RENDITION 480p - Local time %{localtime\: %Y\/%m\/%d %H.%M.%S} (%{n})\':x=10:y=350:fontsize=30:fontcolor=pink:box=1:boxcolor=0x00000099" \
-    -c:v libx264 -b:v 900k -g 30 -profile:v baseline -preset veryfast \
+    -f lavfi -re -i smptebars=duration=36000:size=1280x720:rate=30 \
+    -f lavfi -i sine=frequency=1000:duration=36000:sample_rate=48000 -pix_fmt yuv420p \
+    -vf scale=1280x720 \
+    -vf "drawtext=fontfile=$FONT_PATH:text=\'RENDITION 720p - Local time %{localtime\: %Y\/%m\/%d %H.%M.%S} (%{n})\':x=10:y=350:fontsize=30:fontcolor=pink:box=1:boxcolor=0x00000099" \
+    -c:v libx264 -tune zerolatency -b:v 6000k -g 30 -preset ultrafast \
     -c:a aac -b:a 48k \
-    -f mpegts "$BASE_DIR/fifo-480p"
+    -f mpegts "$BASE_DIR/$FIFO_FILENAME"
 else
     # Start transmuxer
     ffmpeg -hide_banner -y \
     -listen 1 -i "rtmp://0.0.0.0:$RTMP_PORT/$RTMP_APP/$RTMP_STREAM" \
     -c:v copy -c:a copy \
-    -f mpegts "$BASE_DIR/fifo-480p"
+    -f mpegts "$BASE_DIR/$FIFO_FILENAME"
 fi
 
 # Clean up: Stop processes
 # If the input stream stops the segmenter processes exists themselves
-# kill $PID_480p
+# kill $PID_SOURCE
