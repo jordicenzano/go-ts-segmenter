eef6eff23faa7d22a5e88793cfea1023bcc755e9
Jordi Cenzano Ferret <jcenzano@fb.com>
2021-08-30 15:40:26 +0200

    Added file to http script

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/jordicenzano/go-ts-segmenter
+
+go 1.17
+
+require (
+	github.com/aws/aws-sdk-go v1.40.32
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+)
---
+++ scripts/transmuxed-file-to-http.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+	echo "Use ./transmuxed-file-to-http.sh FILEin URLhost [Loop]\n"
+    echo "FILEin: Input file"
+    echo "URLbase: Destination base URL for media chunks (default \"localhost:9094\""
+    echo "Loop: Use file loop (Default 0)"
+    echo "Example: ./transmuxed-file-to-http.sh ~/test.mp4 http://localhost:9094/ID1 1"
+    exit 1
+fi
+
+# Source file
+SRC_FILE=$1
+
+# Dest host
+DST_HOST="${2:-"localhost:9094"}"
+
+# Set loop command
+IS_LOOP="${5:-"0"}"
+LOOP_CMD=""
+if [ "$IS_LOOP" -eq "1" ]; then
+    LOOP_CMD="-stream_loop -1"
+fi
+
+echo "Output to: ${DST_PATH}"
+
+# Starts segmenter 
+../bin/go-ts-segmenter -inputType 2 -targetDur 4 -manifestDestinationType 0 -mediaDestinationType 3 -host $DST_HOST -chunksBaseFilename source_ &
+PID_SRC=$!
+echo "Started go-ts-segmenter for source as PID $PID_SRC"
+
+# Allow server to start
+sleep 2
+
+# Start RTMP listener / transmuxer
+ffmpeg -hide_banner -y \
+-re $LOOP_CMD -i $SRC_FILE \
+-c:v copy -c:a copy \
+-f mpegts "tcp://localhost:2002"
