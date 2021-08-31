51386f4c7663d19f52cf4a65deae3e67cf821176
Jordi Cenzano Ferret <jcenzano@fb.com>
2021-08-31 13:24:56 +0200

    Better script from file to http

--- scripts/transmuxed-file-to-http.sh
+++ scripts/transmuxed-file-to-http.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 if [ $# -lt 1 ]; then
-	echo "Use ./transmuxed-file-to-http.sh FILEin URLhost [Loop]\n"
+	echo "Use ./transmuxed-file-to-http.sh FILEin URLhost [streamID] [Loop]\n"
     echo "FILEin: Input file"
-    echo "URLbase: Destination base URL for media chunks (default \"localhost:9094\""
+    echo "URLhost: Host-port to send the chunks via HTTP (default \"localhost:9094\""
+    echo "streamID: StreamID to use, it is also the path for HTTP requests (default \"DATE WITH SECONDS\""
     echo "Loop: Use file loop (Default 0)"
     echo "Example: ./transmuxed-file-to-http.sh ~/test.mp4 http://localhost:9094/ID1 1"
     exit 1
@@ -15,8 +16,13 @@ SRC_FILE=$1
 # Dest host
 DST_HOST="${2:-"localhost:9094"}"
 
+# Output path (StreamID)
+STREAM_ID_DEF=`date '+%Y%m%d%H%M%S'`
+STREAM_ID="${3:-"$STREAM_ID_DEF"}"
+DST_PATH="ingest/$STREAM_ID"
+
 # Set loop command
-IS_LOOP="${5:-"0"}"
+IS_LOOP="${3:-"0"}"
 LOOP_CMD=""
 if [ "$IS_LOOP" -eq "1" ]; then
     LOOP_CMD="-stream_loop -1"
@@ -25,7 +31,7 @@ fi
 echo "Output to: ${DST_PATH}"
 
 # Starts segmenter 
-../bin/go-ts-segmenter -inputType 2 -targetDur 4 -manifestDestinationType 0 -mediaDestinationType 3 -host $DST_HOST -chunksBaseFilename source_ &
+../bin/go-ts-segmenter -inputType 2 -targetDur 4 -manifestDestinationType 0 -mediaDestinationType 3 -host $DST_HOST -dstPath $DST_PATH -chunksBaseFilename source_ &
 PID_SRC=$!
 echo "Started go-ts-segmenter for source as PID $PID_SRC"
 
